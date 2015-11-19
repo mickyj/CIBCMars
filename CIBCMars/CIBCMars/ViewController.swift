d90bdcf061ac2c6098101a07981129ddbc1ad4df
mickyj <MikeJarrell@Johns-MacBook-Pro.local>
2015-11-19 09:40:50 -0500

    initial commit

---
+++ CIBCMars/CIBCMars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIBCMars
+//
+//  Created by John Jarrell on 2015-11-19.
+//  Copyright © 2015 John Jarrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
